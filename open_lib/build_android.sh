5b7d01779ca4fd5359da02bc8ca6b76559aef427
王永杰 <wangyongjie@kugou.net>
2019-01-17 19:16:57 +0800

    feat:编译4.1版本的ffmpeg

---
+++ open_lib/build_android.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+export STAND_ALONE=../android-toolchain/arm
+export SYSROOT=$STAND_ALONE/sysroot
+export CPU=arm
+export PREFIX=$(pwd)/android/$CPU
+export ADDI_CFLAGS="-marm"
+
+./configure \
+--target-os=linux \
+--prefix=$PREFIX \
+--arch=arm \
+--disable-doc \
+--enable-shared \
+--disable-static \
+--disable-yasm \
+--disable-symver \
+--enable-gpl \
+--disable-ffmpeg \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-doc \
+--disable-symver \
+--cross-prefix=$STAND_ALONE/bin/arm-linux-androideabi- \
+--strip=$STAND_ALONE/bin/arm-linux-androideabi-strip \
+--enable-cross-compile \
+--sysroot=$SYSROOT \
+--extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+--extra-ldflags="$ADDI_LDFLAGS"
+
+make clean
+make
+make install
---
+++ open_lib/make_standalone_toolchain.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+# export NDK_HOME=/workspace/android-ndk-r10e
+platform=android-21
+shmake=$NDK_HOME/build/tools/make-standalone-toolchain.sh
+
+archs=(
+    'arm'
+    'arm64'
+)
+
+toolchains=(
+    'arm-linux-androideabi-4.9'
+    'aarch64-linux-android-4.9'
+)
+
+echo $NDK_HOME
+num=${#archs[@]}
+for ((i=0;i<$num;i++))
+do
+   sh $shmake --arch=${archs[i]} --platform=$platform --install-dir=./android-toolchain/${archs[i]} --toolchain=${toolchains[i]} --stl=stlport
+done
