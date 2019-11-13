aa288514ac8f5b0540913fa5e74f15cd3b5e87cf
王永杰 <wangyongjie@kugou.net>
2019-11-13 08:02:32 +0800

    commit engin

---
+++ LightWeightAnimationEngine/app/src/main/java/com/example/lightweightanimationengine/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lightweightanimationengine;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.Bundle;
+
+import com.android.templateApp.templateApp;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private templateApp tApp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        tApp = new templateApp(this);
+    }
+
+    @Override protected void onResume()
+    {
+        super.onResume();
+        tApp.onResume();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        tApp.onSensorChanged(event);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        tApp.onAccuracyChanged(sensor, i);
+    }
+}
